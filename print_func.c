34686552b2fba963ad91c1727e931419d142627c
Loubnaa1 <loubnamerzak4@gmail.com>
2023-06-26 08:23:26 -0700

    Commit

--- print_func.c
+++ print_func.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
-char print_char(va_list args, char c)
+char print_char(char c)
 {
+	return (write(1, va_args(args, i, 1));
 
 }
+
+char print_percent(va_list args)
+{ 
+	c == '%';
+	print_char(c);
+}
+
+char print_string(va_list args, char *string)
+{ 
+        int i 
+}   
