3a5850906137f37d310d825bc4ddff6b12f20142
gunner206 <gunner.alx@hmail.com>
2023-06-27 07:29:23 -0700

    2

--- convert.c
+++ convert.c
@@ -30,6 +30,17 @@ int convert(const char *format, specifier_t func_list[], va_list args)
 					break;
 				}
 			}
+			if (func_list[j].specifier == NULL && format[i + 1] == ' ')
+			{
+				if (format[i] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					printed_chars = printed_chars + 2;
+				}
+				else
+					return (-1);
+			}
 			i = i + 1;
 		}
 		else
