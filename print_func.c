2d31b3c1935cbc1d69df286f94fe3228949886c3
loubnaa1 <loubnamerzak4@gmail.com>
2023-06-27 07:12:44 -0700

    commit

--- print_func.c
+++ print_func.c
@@ -7,6 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
@@ -18,13 +19,23 @@ int print_char(va_list args)
 	_putchar(c);
 	return (1);
 }
+/**
+ * print_percent - function that print the character
+ * @args: character to pass
+ * Return: On success 1.
+ */
+
 int print_percent(va_list args)
 {
 	(void)args;
 	_putchar('%');
 	return (1);
 }
-
+/**
+ * print_string - function that print a string
+ * @args: pointer to first argument
+ * Return: number of characters of the string
+ */
 int print_string(va_list args)
 {
 	const char *p;
